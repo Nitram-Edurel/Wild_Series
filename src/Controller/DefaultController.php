91ebf686005cec36cb067679ec4cc90838b7caa5
Martin Lerude <martin.lerude@gmail.com>
2021-11-24 22:22:07 +0100

    Symfony 3

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+//src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultCotroller extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'title' => 'Bienvenue !',
+        ]);
+    }
+}
