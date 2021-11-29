1ca0be35bbb18de2df6e82a83935789d63f3d07e
Martin Lerude <martin.lerude@gmail.com>
2021-11-29 15:30:44 +0100

    Symfony 5

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultCotroller extends AbstractController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,20 +1,33 @@
 <?php
-//src/Controller/ProgramController.php
+// src/Controller/ProgramController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program_")
+ */
 class ProgramController extends AbstractController
 {
+
     /**
      * @Route("/program/", name="program_index")
+     * @return Response
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>", methods={"POST"}, name="program_id")
+     */
+    public function show($id): Response
+    {
+        return $this->render('program/show.html.twig');
     }
 }
